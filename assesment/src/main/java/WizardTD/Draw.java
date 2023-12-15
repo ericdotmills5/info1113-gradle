d4d87999997bcdb050c1dbc70a97102c28c67504
emil0927 <emil0927@uni.sydney.edu.au>
2023-12-15 12:48:30 +1100

    merged branches, ready to upload

--- assesment/src/main/java/WizardTD/Draw.java
+++
@@ -1,9 +0,0 @@
-package WizardTD;
-
-public interface Draw {
-
-    /**
-     * draw function for all objects to be implemented individually
-     */
-    public void draw(App inputApp);
-}
--- assesment/src/main/java/WizardTD/Tick.java
+++
@@ -1,8 +0,0 @@
-package WizardTD;
-
-public interface Tick {
-    /**
-     * tick function for all objects to be implemented individually
-     */
-    public void tick(App inputApp);
-}
